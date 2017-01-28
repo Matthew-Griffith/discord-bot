772fb8b0a108500b0b09490d0c917aa1eba8a94b
Matthew-Griffith <matt.griff10@gmail.com>
2017-01-28 17:38:26 -0500

    Update bot.js
    this is most latest version of the bot. probably the final version

--- bot.js
+++ bot.js
@@ -2,11 +2,21 @@
 var Discord = require("discord.js");
 var moment = require("moment-timezone");
 var ddg = require("ddg");
+var YouTube = require('youtube-node');
 
+var youtube = new YouTube();
+youtube.setKey('your_youtube_api_key');
 var bot = new Discord.Client();
 
+// function for a random int made for the !roll command
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 bot.on("message", (message) => {
-    //this is the prefix at the start of all  the commands
+    // this is the prefix at the start of all the commands
     let prefix = "!";
     // this an object that will content the commands and responses
     var responseObject = {
@@ -20,7 +30,7 @@ bot.on("message", (message) => {
         "!love": "http://i.imgur.com/8ZsL61S.gif",
         "!fire": "http://i.imgur.com/6XcSabd.png",
         "!mmm": "http://i.imgur.com/Ubjkvpd.gif",
-        "!help": "list of commands:\n!laughing\n!shitposting\n!calm\n!anime\n!itsokay\n!what\n!embarrassed\n!love\n!fire\n!mmm\n!time: this command will give the time and data of a list of cities see !cities\n!geneticfreak\n!answer takes a user input and tries to tell you about it, for all the time you have thought what the fuck is he talking about.",
+        "!help": "list of commands:\n!laughing\n!shitposting\n!calm\n!anime\n!itsokay\n!what\n!embarrassed\n!love\n!fire\n!mmm\n!time: this command will give the time and data of a list of cities see !cities\n!geneticfreak\n!answer takes a user input and tries to tell you about it, for all the time you have thought what the fuck is he talking about.\n!yt: input a search and returns the top youtube result.\n!roll: I can flip coins, roll: d4, d6, d8, d10, d12 and d20",
         "!cities": "List of cities:\nnew york\nchicago\ndenver\nlos angeles\nlondon\nparis\nbucharest\nmoscow\ndubai\nbeijing\nseoul\ntokyo",
         "!geneticfreak": "https://www.youtube.com/watch?v=WFoC3TR5rzI"
     }
@@ -50,11 +60,14 @@ bot.on("message", (message) => {
       below is the implementation for the time zone command.
 
       TODO: try to generize this so that the users can just in most major cities without
-      have to actually write all of them in the object above.
+            have to actually write all of them in the object above.
     */
     if(message.content.startsWith(prefix + "time")) {
       if(timezone_data[message.content.toLowerCase()]) {
         message.channel.sendMessage(moment.tz(timezone_data[message.content.toLowerCase()]).format('MM-DD HH:mm'));
+      }
+      else {
+        message.channel.sendMessage('Please check for typos in your query, and make sure the city is in the !cities list.')
       }
     }
 
@@ -62,7 +75,8 @@ bot.on("message", (message) => {
       below is the answer command implementation it searchs duck duck go and try to
       given a small useful Abstract for the user.
 
-      TODO: remove safe search.
+      TODO: look into bug with duckduckgo instant answers working on the website but not for the bot.
+            overwatch as a query is an example of this.
     */
     if (message.content.startsWith(prefix + "answer")) {
       var user_search = message.content.slice(8, message.content.length);
@@ -70,11 +84,68 @@ bot.on("message", (message) => {
         if (data.Abstract === '') {
           message.channel.sendMessage("I don't know... #blameMatt");
         }
+        else if (err) {
+          message.channel.sendMessage("oh god, there was an error.");
+        }
         else {message.channel.sendMessage(data.Abstract);}
       });
     }
 
-    // this is the logic for the simple commands.
+    /*
+      this command will search youtube and return the top results and post for people in the 
+      chat.
+
+      TODO: try to get a few search results and display the 2nd or 3rd result when the first result
+            is a playlist or channnel.
+    */
+    if (message.content.startsWith(prefix + "yt")) {
+      var user_search = message.content.slice(4, message.content.length);
+      youtube.search(user_search, 1, function(error, result){
+        if (error) {
+          message.channel.sendMessage("oh god there was an error");
+          //console.log(error);
+        }
+        else if (result.items[0].id.videoId === undefined){
+          message.channel.sendMessage("sorry I can't display the results of your search.")
+        }
+
+        else {
+          message.channel.sendMessage("http://www.youtube.com/watch?v=" + result.items[0].id.videoId);
+        }
+      });
+    }
+
+    /*
+      this is a command to "roll" dice. it pretty straigth forward will use getRandomInt
+      function the created in the begin of this file. 
+
+      TODO: sometimes I am getting promise errors but I think I fixed them however, 
+            I am not sure what is causing them in the first place. it might be here or
+            in the !yt command. 
+    */
+    if (message.content.startsWith(prefix + "roll")) {
+      var user_input = message.content.slice(6, message.content.length);
+      var dice = {
+        "d4" : "You rolled... " + getRandomInt(1,4),
+        "d6" : "You rolled... " + getRandomInt(1,6),
+        "d8" : "You rolled... " + getRandomInt(1,8),
+        "d10" : "You rolled... " + getRandomInt(1, 10),
+        "d12" : "You rolled... " + getRandomInt(1,12),
+        "d20" : "You rolled... " + getRandomInt(1,20)
+      }
+      if (user_input.toLowerCase() === 'coin') {
+        if (getRandomInt(0, 1) === 1) {
+          message.channel.sendMessage('It is... Tails');
+        }
+        else {message.channel.sendMessage('It is... Heads');}
+      }
+      else if (dice[user_input]) {
+        message.channel.sendMessage(dice[user_input]);
+      }
+      else {message.channel.sendMessage("I cannot roll that!!!");}
+    }
+
+    // this is the logic for the commands
     if(responseObject[message.content]) {
         message.channel.sendMessage(responseObject[message.content]);
     }
@@ -84,4 +155,5 @@ bot.on('ready', () => {
   console.log('I am ready!');
 });
 
-bot.login("bot_token_goes_here");
+bot.login("your_bot_token");
+
